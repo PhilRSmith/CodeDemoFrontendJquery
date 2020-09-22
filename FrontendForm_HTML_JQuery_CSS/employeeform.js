871258c6c0ed39b4830efdb49d531cc733d7026a
PhilRSmith <theonepudgypanda@gmail.com>
2020-09-22 13:39:38 -0400

    frame made, input handling needed

--- FrontendForm_HTML_JQuery_CSS/employeeform.js
+++ FrontendForm_HTML_JQuery_CSS/employeeform.js
@@ -1,3 +1,25 @@
-$(document).ready(function() {
-    
-    });
+$(document).ready (function(){
+    $("#submit").click(function(){
+        var empName = $('#employeeName').val();
+        var empNumber = $('#employeeNumber').val();
+        var phoneNumber = $('#phoneNumber').val();
+        $("#returnmessage").empty(); //clear server response message
+        $("#returnmessage").append(empName + " : " , empNumber + " : ", phoneNumber); /*Just a test of input fields*/
+        $.ajax({
+            type: "POST",
+            url: "fictitious ",
+            data: {
+                employeeName : empName , 
+                employeeNumber : empNumber ,
+                phone : phoneNumber
+            },
+            dataType: "json"
+        },function(res){
+            /*On success, returns a success message into #returnmessage from the server, since server doesn't exist, this won't happen*/
+            $("#returnmessage").append(res);
+        }
+        )
+    })
+})
+
+
