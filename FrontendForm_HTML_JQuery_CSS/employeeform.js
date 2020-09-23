5a2261b3e9d7ab3c46cf8a0208e0f02531cce892
PhilRSmith <theonepudgypanda@gmail.com>
2020-09-23 12:09:04 -0400

    basic form input handling done|todo: post req test

--- FrontendForm_HTML_JQuery_CSS/employeeform.js
+++ FrontendForm_HTML_JQuery_CSS/employeeform.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    
+    var inputState='';
     /*InputMask Library functions by Josh Bush */
     var phoneMasks = [{ "mask": "(###) ###-####"}, { "mask": "(###) ###-##############"}]; 
     var zipcodeMask = [{"mask" : "#####"}];
@@ -14,41 +14,54 @@ $(document).ready(function(){
         definitions: { '#': { validator: "[0-9]", cardinality: 1}}
     });
 
+    /*grab combo box value on change*/
+    $('input[name = "state"]').on('input' , function(e){ 
+        inputState = $(this).val();
+    })
+
     /*On clicking submit, send post req to server with form information, then return server success/error msg*/
     $("#submit").click(function(){
         var inputEmpName = $('#employeeName').val();
         var inputEmpNumber = $('#employeeNumber').val();
         var inputPhoneNumber = $('#phoneNumber').val();
-        var inputState = $('#state').val();
+        //var inputState = $('#state').val();
         var inputZipCode = $('#zipCode').val();
         var inputStreetAddress = $('#address').val(); 
         var inputCity = $('#city').val();
-        var isEmployee = $('#isEmployee').is(':checked')
+        var isEmployee = $('#isEmployee').is(':checked');
+
         $("#returnmessage").empty(); //clear server response message
-        $("#returnmessage").append(inputEmpName + " : " , inputEmpNumber + " : ", inputPhoneNumber); /*Just a test of input fields*/
-        $.ajax({
-            type: "POST",
-            url: "https://fictitious.notreal",
-            data: JSON.stringify({
-                'employeeName' : inputEmpName , 
-                'employeeNumber' : inputEmpNumber ,
-                'phone' : inputPhoneNumber,
-                'zipCode' : inputZipCode,
-                'state' : inputState,
-                'address' : inputStreetAddress,
-                'city' : inputCity,
-                'isEmployee' : isEmployee
-            }),
-            dataType: "json",
-            success: function(data, status){
-                $('#returnmessage').append(JSON.stringify(data));
-                console.log(status);
-            },
-            error: function (error, status){
-                console.log(error);
-                console.log(status);
-            }
-        })
+        $("#errormessage").empty();
+        if(inputEmpName=='' || inputEmpNumber =='' || inputPhoneNumber =='' || inputState=='' 
+        ||inputZipCode==''||inputStreetAddress==''||inputCity==''){
+            $('#errormessage').append('Please enter your information for all fields before submitting');
+        }
+        else {
+            //$('#returnmessage').append(inputEmpName + ' | ' ,inputEmpNumber + ' | ' , inputPhoneNumber + ' | ' ,inputState + ' | ' ,inputZipCode + ' | ' ,inputStreetAddress + ' | ' ,inputCity + ' | ' ,isEmployee)
+            $.ajax({
+                type: "POST",
+                url: "https://fictitious.notreal",
+                data: JSON.stringify({
+                    'employeeName' : inputEmpName , 
+                    'employeeNumber' : inputEmpNumber ,
+                    'phone' : inputPhoneNumber,
+                    'zipCode' : inputZipCode,
+                    'state' : inputState,
+                    'address' : inputStreetAddress,
+                    'city' : inputCity,
+                    'isEmployee' : isEmployee
+                }),
+                dataType: "json",
+                success: function(data, status){
+                    $('#returnmessage').append(JSON.stringify(data));
+                    console.log(status);
+                },
+                error: function (error, status){
+                    console.log(error);
+                    console.log(status);
+                }
+            })
+        }
     })
 })
 
